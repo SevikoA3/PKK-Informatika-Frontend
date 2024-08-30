6f19c509fcdd356eaacad8306dfa63128a30616d
SevikoA3 <attalarik.handoko@gmail.com>
2024-08-30 16:29:46 +0700

    revise: mahasigma

--- src/Components/mahasigma.jsx
+++ src/Components/mahasigma.jsx
@@ -1,10 +1,10 @@
 export default function Mahasigma({ no, name, kelompok, image }) {
     return (
         <div className="bg-custom-gradient3 rounded-lg w-full text-white flex justify-between pl-4 pt-2 relative shadow-2xl">
-            <div className="flex flex-col gap-5">
-                <div className="flex">
+            <div className="flex flex-col gap-7">
+                <div className="flex pt-3">
                     <p className="text-2xl">#</p>
-                    <p className="text-5xl font-bold">{no}</p>
+                    <p className="text-3xl md:text-4xl font-bold relative z-30">{no < 2 ? "King👑": "Queen✨"}</p>
                 </div>
                 <div className="max-w-1/2 tracking-tighter">
                     <p className="text-xl font-semibold relative z-30">{name}</p>
@@ -12,7 +12,7 @@ export default function Mahasigma({ no, name, kelompok, image }) {
                 </div>
             </div>
             <img src={image} alt="mahasigma" className="rounded-lg h-[10rem] absolute bottom-0 right-0" />
-            <p className="p-1 bg-[#E85C0D] text-[#FABC3F] absolute tracking-tighter bottom-0 right-0 text-xs rounded-md">+{no < 2 ? 10000: 5000} Aura</p>
+            <p className="p-1 bg-[#E85C0D] text-[#FABC3F] absolute tracking-tighter bottom-0 right-0 text-xs rounded-md">+10000 Aura</p>
         </div>
     );
 }
