d640cf04628ca87dfda91beb963e4ba4b38414b7
SevikoA3 <attalarik.handoko@gmail.com>
2024-08-30 11:38:21 +0700

    fix: mahasigma card

--- src/Components/mahasigma.jsx
+++ src/Components/mahasigma.jsx
@@ -7,12 +7,12 @@ export default function Mahasigma({ no, name, kelompok, image }) {
                     <p className="text-5xl font-bold">{no}</p>
                 </div>
                 <div className="max-w-1/2 tracking-tighter">
-                    <p className="text-xl">{name}</p>
+                    <p className="text-xl font-semibold">{name}</p>
                     <p className="text-sm pb-2">{kelompok}</p>
                 </div>
             </div>
             <img src={image} alt="mahasigma" className="rounded-lg h-[10rem] absolute bottom-0 right-0" />
-            <p className="p-1 bg-[#E85C0D] text-[#FABC3F] absolute tracking-tighter bottom-0 right-0 text-xs rounded-md">+10000 Aura</p>
+            <p className="p-1 bg-[#E85C0D] text-[#FABC3F] absolute tracking-tighter bottom-0 right-0 text-xs rounded-md">+{no < 2 ? 10000: 5000} aura</p>
         </div>
     );
 }
